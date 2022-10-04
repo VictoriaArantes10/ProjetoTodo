3ccd3d6f765e26e1711bd7141bfcd03a33c946d1
Victoria <victoria@prodez.com.br>
2022-10-04 08:44:00 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Tarefa.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace toDo.Models
+{
+    public class Tarefa
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        public bool Status { get; set; }
+    }
+    
+
+
+
+    
+}
